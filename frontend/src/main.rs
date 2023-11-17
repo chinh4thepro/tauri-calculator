3c98940a9e23a9fe79c6b6224d7b269102b40f31
chinh4thepro <chinh4thepro@gmail.com>
2023-11-16 22:16:39 -0500

    make ui actually readable

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,43 +6,31 @@ fn main() {
 
 #[function_component]
 pub fn App() -> Html {
-    html! {
+    html! { 
         <div class="dark:bg-black container m-10 mx-auto flex h-screen w-screen rounded-xl bg-gray-600 p-10 drop-shadow-lg">
-            <table class="w-full">
-                <tr>
-                    <td colspan="4"><input type="text" id="result" class="w-full bg-black text-white rounded-xl" /></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="AC"/></td>
-                    <td><input type="button" value="()"/></td>
-                    <td><input type="button" value="%"/></td>
-                    <td><input type="button" value="/"/></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="7"/></td>
-                    <td><input type="button" value="8"/></td>
-                    <td><input type="button" value="9"/></td>
-                    <td><input type="button" value="*"/></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="4"/></td>
-                    <td><input type="button" value="5"/></td>
-                    <td><input type="button" value="6"/></td>
-                    <td><input type="button" value="-"/></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="1"/></td>
-                    <td><input type="button" value="2"/></td>
-                    <td><input type="button" value="3"/></td>
-                    <td><input type="button" value="+"/></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="0"/></td>
-                    <td><input type="button" value="."/></td>
-                    <td><input type="button" value="back"/></td>
-                    <td><input type="button" value="="/></td>
-                </tr>
-            </table>
+            <div class="grid gap-2 grid-cols-4 grid-rows-5 w-full h-full">
+                <input type="text" id="result" class="w-full bg-black text-white rounded-xl col-span-4" />
+                <button class="bg-gray-400 rounded-lg">{"AC"}</button>
+                <button class="bg-gray-200 rounded-lg">{"()"}</button>
+                <button class="bg-gray-200 rounded-lg">{"%"}</button>
+                <button class="bg-gray-200 rounded-lg">{"÷"}</button>
+                <button class="bg-gray-200 rounded-lg">{"7"}</button>
+                <button class="bg-gray-200 rounded-lg">{"8"}</button>
+                <button class="bg-gray-200 rounded-lg">{"9"}</button>
+                <button class="bg-gray-200 rounded-lg">{"X"}</button>
+                <button class="bg-gray-200 rounded-lg">{"4"}</button>
+                <button class="bg-gray-200 rounded-lg">{"5"}</button>
+                <button class="bg-gray-200 rounded-lg">{"6"}</button>
+                <button class="bg-gray-200 rounded-lg">{"-"}</button>
+                <button class="bg-gray-200 rounded-lg">{"1"}</button>
+                <button class="bg-gray-200 rounded-lg">{"2"}</button>
+                <button class="bg-gray-200 rounded-lg">{"3"}</button>
+                <button class="bg-gray-200 rounded-lg">{"+"}</button>
+                <button class="bg-gray-200 rounded-lg">{"0"}</button>
+                <button class="bg-gray-200 rounded-lg">{"."}</button>
+                <button class="bg-gray-200 rounded-lg">{"B"}</button>
+                <button class="bg-gray-200 rounded-lg">{"="}</button>
+            </div>
         </div>
     }
 }
