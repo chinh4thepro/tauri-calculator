79107c50981f69f86c4e0397bbdae5f2e51f4c46
chinh4thepro <chinh4thepro@gmail.com>
2023-12-03 23:22:39 -0500

    I "finished" it and i will probably never touch this again

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     yew::Renderer::<App>::new().render();
 }
 
+
 // Entire code for calculator
 #[function_component]
 pub fn App() -> Html {
@@ -16,7 +17,7 @@ pub fn App() -> Html {
     }
 
     // Layout for the equation and determine types
-    #[derive(Clone)]
+    #[derive(Clone, Debug)]
     struct Equation {
         inputone: String,
         inputtwo: String,
@@ -29,7 +30,7 @@ pub fn App() -> Html {
     }
 
     // Find out what operation it is
-    #[derive(Clone)]
+    #[derive(Clone, Debug)]
     enum Operation {
         Addition,
         Subtraction,
@@ -65,14 +66,20 @@ pub fn App() -> Html {
 
     let equation = use_state(|| Equation::default());
     let equation_inputs = use_state_eq(|| EquationInputs::InputOne);
+    let result = use_state(String::new);
     let button_pressed = {
         let equation = equation.clone();
         let equation_inputs = equation_inputs.clone();
+        let result = result.clone();
         Callback::from(move |event: MouseEvent| {
             // Find out what button was pressed
             let value: String = event.target().unwrap().dyn_into::<HtmlButtonElement>().unwrap().value();
             let is_operation: bool = match format!("{value}").as_str() {
                 "+" | "-" | "/" | "*" | "%" => true,
+                _ => false
+            };
+            let is_equal: bool = match format!("{value}").as_str() {
+                "=" => true,
                 _ => false
             };
 
@@ -86,34 +93,40 @@ pub fn App() -> Html {
                     EquationInputs::InputTwo => { new_equation.inputtwo.push_str(&value); },
                 }
             }
+
             equation.set(new_equation);
+
+            if is_equal {
+               result.set(equation.operation.clone().unwrap().maths(equation.inputone.parse().unwrap(), equation.inputtwo.parse().unwrap()).to_string()) 
+            }
+
         })
     };
 
     html! { 
         <div class="dark:bg-black container m-10 mx-auto flex h-screen w-screen rounded-xl bg-gray-600 p-10 drop-shadow-lg">
             <div class="grid gap-2 grid-cols-4 grid-rows-5 w-full h-full">
-                <input value={ (*equation).clone() } type="text" id="result" class="w-full bg-black text-white rounded-xl col-span-4" />
+                <input type="text" value={ format!("{:?}", *result.clone()) } id="result" class="w-full bg-black text-white rounded-xl col-span-4" />
                 <button class="bg-gray-400 rounded-lg">{"C"}</button>
                 <button class="bg-gray-200 rounded-lg">{"()"}</button>
-                <button class="bg-gray-200 rounded-lg" value="%">{"%"}</button>
-                <button class="bg-gray-200 rounded-lg" value="/">{"÷"}</button>
-                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed } value="7">{"7"}</button>
-                <button class="bg-gray-200 rounded-lg" value="8">{"8"}</button>
-                <button class="bg-gray-200 rounded-lg" value="9">{"9"}</button>
-                <button class="bg-gray-200 rounded-lg" value="*">{"×"}</button>
-                <button class="bg-gray-200 rounded-lg" value="4">{"4"}</button>
-                <button class="bg-gray-200 rounded-lg" value="5">{"5"}</button>
-                <button class="bg-gray-200 rounded-lg" value="6">{"6"}</button>
-                <button class="bg-gray-200 rounded-lg" value="-">{"-"}</button>
-                <button class="bg-gray-200 rounded-lg" value="1">{"1"}</button>
-                <button class="bg-gray-200 rounded-lg" value="2">{"2"}</button>
-                <button class="bg-gray-200 rounded-lg" value="3">{"3"}</button>
-                <button class="bg-gray-200 rounded-lg" value="/">{"+"}</button>
-                <button class="bg-gray-200 rounded-lg" value="0">{"0"}</button>
-                <button class="bg-gray-200 rounded-lg" value=".">{"."}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="%">{"%"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="/">{"÷"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() } value="7">{"7"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="8">{"8"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="9">{"9"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="*">{"×"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="4">{"4"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="5">{"5"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="6">{"6"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="-">{"-"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="1">{"1"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="2">{"2"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="3">{"3"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="+">{"+"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value="0">{"0"}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() }  value=".">{"."}</button>
                 <button class="bg-gray-200 rounded-lg">{"⌫"}</button>
-                <button class="bg-gray-200 rounded-lg">{"="}</button>
+                <button class="bg-gray-200 rounded-lg" onclick={ button_pressed.clone() } value="=">{"="}</button>
             </div>
         </div>
     }
